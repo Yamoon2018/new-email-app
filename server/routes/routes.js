9a538a3d8aa3054721f16b3d15e9dac7f4f1f3a8
Yamoon2018 <yamin.barakat@gmail.com>
2020-11-20 05:55:52 +0400

    Email modifications

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const email_template = require('../models/email-template');
-const send_email_db = require('./send_email_smtp')
+const send_email_db = require('./send_email_smtp');
 
 router.post('/save_db', (req, res)=>{
-    console.log(req.body);
+    console.log("req body =="+req.body);
     send_email_db(req.body);
     const new_email_template = new email_template({
         sender_name: req.body.sender_name,
